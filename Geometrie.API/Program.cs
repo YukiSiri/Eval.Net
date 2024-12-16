fb6f52eadc8d8b0749970b36c60fa54cdea27dcf
Alexandre <alexandre.levenez@ecoles-epsi.net>
2024-12-16 19:25:46 +0100

    ajout du projet

---
+++ Geometrie.API/Program.cs
@@ -0,0 +1,33 @@
+using Geometrie.DAL;
+using Geometrie.DTO;
+using Geometrie.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//pour ajouter un service dans le conteneur d'injection de dépendances
+builder.Services.AddSingleton<IPoint_Service>(new Point_Service(new GeometrieContext()));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
