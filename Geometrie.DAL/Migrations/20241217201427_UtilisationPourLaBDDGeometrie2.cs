0b134ede2eedd99a6da3f0f78433737146f65416
Alexandre <alexandre.levenez@ecoles-epsi.net>
2024-12-17 21:17:20 +0100

    Changement de BDD

---
+++ Geometrie.DAL/Migrations/20241217201427_UtilisationPourLaBDDGeometrie2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Geometrie.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UtilisationPourLaBDDGeometrie2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
